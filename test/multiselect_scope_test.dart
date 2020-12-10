59f854465eaf06a2a073a13254209d85203f36d6
boris-home <boris-home@home.com>
2020-12-10 23:32:56 +0800

    Prebuild test

--- test/multiselect_scope_test.dart
+++ test/multiselect_scope_test.dart
@@ -1,11 +1,206 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:multiselect_scope/multiselect_scope.dart';
 
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
-    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  testWidgets('Multiselect test', (WidgetTester tester) async {
+    await tester.pumpWidget(MyAppTest());
   });
+}
+
+class MyAppTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Multiselect scope Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePageTest(title: 'Multiselect'),
+    );
+  }
+}
+
+class MyHomePageTest extends StatefulWidget {
+  MyHomePageTest({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageTestState createState() => _MyHomePageTestState();
+}
+
+class _MyHomePageTestState extends State<MyHomePageTest> {
+  List<String> _items;
+  MultiselectController _multiselectController;
+  Random random;
+
+  @override
+  void initState() {
+    super.initState();
+    random = Random();
+    _items = List.generate(10, (index) => 'Item $index');
+
+    _multiselectController = MultiselectController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: MultiselectScope<String>(
+        controller: _multiselectController,
+        dataSource: _items,
+        clearSelectionOnPop: true,
+        initialSelectedIndexes: [1, 3],
+        onSelectionChanged: (indexes, items) {
+          debugPrint(
+              'Custom listener invoked! Indexes: $indexes Items: $items');
+          return;
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: ListView.builder(
+                    itemCount: _items.length,
+                    itemBuilder: (context, index) {
+                      final controller = MultiselectScope.of(
+                          context); //MultiselectScope.of(context);
+
+                      final itemIsSelected = controller.indexIsSelected(index);
+
+                      return InkWell(
+                        onLongPress: () {
+                          if (!controller.selectionAttached) {
+                            controller.select(index);
+                          }
+                        },
+                        onTap: () {
+                          debugPrint('Item is selected: $itemIsSelected');
+
+                          if (controller.selectionAttached) {
+                            controller.select(index);
+                          }
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: itemIsSelected
+                                ? Theme.of(context).primaryColor
+                                : null,
+                            child: Text(
+                              _items[index],
+                              style: TextStyle(fontSize: 22),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+              Wrap(
+                children: <Widget>[
+                  RawMaterialButton(
+                    child: Text('Add rand'),
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    fillColor: Colors.blueGrey,
+                    onPressed: () {
+                      setState(() {
+                        final randItem =
+                            'RandItem' + random.nextInt(256).toString();
+
+                        final randomIndex = _items.isEmpty
+                            ? 0
+                            : random.nextInt(_items.length - 1);
+                        _items.insert(randomIndex,
+                            randItem); // TODO Fix after delete all!
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Remove rand'),
+                    fillColor: Colors.lightGreen,
+                    onPressed: () {
+                      setState(() {
+                        if (_items.length == 1) {
+                          _items.removeAt(0);
+                        } else {
+                          _items.removeAt(random.nextInt(_items.length - 1));
+                        }
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    fillColor: Colors.blueGrey,
+                    child: Text('Delete'),
+                    onPressed: () {
+                      setState(() {
+                        final itemsToRemove = _multiselectController
+                            .getSelectedItems()
+                            .cast<String>();
+
+                        _items = _items
+                            .where(
+                                (element) => !itemsToRemove.contains(element))
+                            .toList();
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Select 0'),
+                    fillColor: Colors.lightGreen,
+                    onPressed: () {
+                      setState(() {
+                        _multiselectController.select(0);
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Select all'),
+                    fillColor: Colors.amber,
+                    onPressed: () {
+                      setState(() {
+                        _multiselectController.selectAll();
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Invert'),
+                    fillColor: Colors.tealAccent,
+                    onPressed: () {
+                      setState(() {
+                        _multiselectController.invertSelection();
+                      });
+                    },
+                  ),
+                  RawMaterialButton(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text('Clear'),
+                    fillColor: Colors.deepPurpleAccent,
+                    onPressed: () {
+                      setState(() {
+                        _multiselectController.clearSelection();
+                      });
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
